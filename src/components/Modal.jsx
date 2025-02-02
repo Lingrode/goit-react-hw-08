c6dafe8e3d385247270ea78bd3e16851c365462e
Illia Hrymalo <grimaloilya@gmail.com>
2025-02-02 00:27:17 -0700

    change modal window

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,38 +3,47 @@ import PropTypes from "prop-types";
 
 const Modal = ({ isOpen, close, handleDelete }) => {
   useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        close();
+      }
+    };
+
     if (isOpen) {
       document.body.classList.add("overflow-hidden");
+      document.addEventListener("keydown", handleKeyDown);
     } else {
       document.body.classList.remove("overflow-hidden");
     }
 
     return () => {
       document.body.classList.remove("overflow-hidden");
+      document.removeEventListener("keydown", handleKeyDown);
     };
-  }, [isOpen]);
+  }, [isOpen, close]);
 
   return (
     <div
       className={`${
-        isOpen ? "block" : "hidden"
-      } bg-black/50 p-5 fixed inset-0 flex items-center justify-center`}
+        isOpen ? "visible opacity-100" : "invisible opacity-0"
+      } bg-black/50 p-5 fixed inset-0 flex items-center justify-center transition-all cursor-pointer`}
+      onClick={close}
     >
-      <div className="bg-white p-16 rounded-lg shadow-lg ">
+      <div
+        className="bg-white p-16 rounded-lg shadow-lg cursor-auto"
+        onClick={(event) => event.stopPropagation()}
+      >
         <p className="mb-6 text-2xl">
           Are you sure you want to delete the contact?
         </p>
         <div className="flex gap-9 justify-center  ">
           <button
-            className="cursor-pointer px-6 py-2 text-white bg-red-500 hover:bg-red-600 transition-colors rounded-lg"
+            className="btn btn-soft btn-lg btn-error"
             onClick={handleDelete}
           >
             Yes
           </button>
-          <button
-            className="cursor-pointer px-6 py-2 text-black bg-gray-300 hover:bg-gray-400/70 transition-colors rounded-lg"
-            onClick={close}
-          >
+          <button className="btn btn-lg btn-soft" onClick={close}>
             No
           </button>
         </div>
